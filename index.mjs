cd8e5652da2ba7bfb3552f587502faa35e1b4b09
Thiwat-sudo <123875405+Thiwat-sudo@users.noreply.github.com>
2024-05-10 23:58:47 +0700

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,2 +1,2 @@
 
-document.getElementById("app").innerHTML = `<h1>Hello Welcome to my home!</h1>`;
+document.getElementById("app").innerHTML = `<h1>Hello Welcome to my home!</h1><img src='./qq.png'>`;
